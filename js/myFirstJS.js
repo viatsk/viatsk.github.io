fa1d06cc8ebe67957e4059eb286f3e66b20d4f0d
viatsk <viatsk@fastmail.com>
2020-04-30 15:27:03 +0000

    Create navigation header

---
+++ js/myFirstJS.js
@@ -0,0 +1,12 @@
+
+function myFunction() {
+    var x = document.getElementById("landingTopNav")
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";    
+    }
+}
+
+
+
